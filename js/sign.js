b90e05b63bdc468507468cdc9c01c007cc10753a
xingkong <ldk8259878@163.com>
2016-06-17 17:21:42 +0800

    picture links

--- js/sign.js
+++ js/sign.js
@@ -1,38 +1,38 @@
 $('.business-form .btn').click(function () {
 
-    if ($('.business-form .realname').val()=='') {
+    if ($('.business-form .realname').val()==='') {
         alert('姓名不能为空');
         return false;
     }
-    if ($('.business-form .email').val()=='') {
+    if ($('.business-form .email').val()==='') {
         alert('邮箱不能为空');
         return false;
     }
-    if((/^[0-9]{11}$/).test($('.business-form .phone').val())===false){
-        alert('手机格式不正确');
+    if($('.business-form .phone').val()===""){
+        alert('手机不能为空');
         return false;
     }
-    if ($('.business-form .company').val()=='') {
+    if ($('.business-form .company').val()==='') {
         alert('公司不能为空');
         return false;
     }
-    if ($('.business-form .medianame').val()=='') {
+    if ($('.business-form .medianame').val()==='') {
         alert('媒体名称不能为空');
         return false;
     }
-    if ($('.business-form .job').val()=='') {
+    if ($('.business-form .job').val()==='') {
         alert('职位不能为空');
         return false;
     }
-    if ($('.business-form .product').val()=='') {
+    if ($('.business-form .product').val()==='') {
         alert('展示的产品或服务不能为空');
         return false;
     }
-    if ($('.business-form .area').val()=='') {
+    if ($('.business-form .area').val()==='') {
         alert('所需展位大小不能为空');
         return false;
     }
-    if ($('.business-form .url').val()=='') {
+    if ($('.business-form .url').val()==='') {
         alert('公司网站不能为空');
         return false;
     }
